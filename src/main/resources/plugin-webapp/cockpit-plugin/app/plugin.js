f94e7b85c7cd318b64400923b11dd0ed32d00d0a
camunda-jenkins <ci_automation@camunda.com>
2020-11-10 10:47:55 +0100

    Upgrade to 7.14.0

--- src/main/resources/plugin-webapp/cockpit-plugin/app/plugin.js
+++ src/main/resources/plugin-webapp/cockpit-plugin/app/plugin.js
@@ -1,35 +1,42 @@
-define(['angular'], function(angular) {
+function generateHTML(processInstanceCounts) {
+  const body = processInstanceCounts
+    .map(
+      element => `<tr>
+                    <td>${element.key}</td>
+                    <td>${element.instanceCount}</td>
+                  </tr>`
+    )
+    .join("\n");
 
-  var ngModule = angular.module('cockpit.plugin.cockpit-plugin', []);
+  return `
+<section>
+  <div class="inner">
+    <h3>Camunda Cockpit Plugin</h3>
+    <table class="table table-bordered table-hover table-condensed">
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Instances</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${body}
+      </tbody>
+    </table>
+  </div>
+</section>`;
+}
 
-  var DashboardController = function($scope, $http, Uri) {
-
-    $http.get(Uri.appUri("plugin://cockpit-plugin/:engine/process-instance"))
-      .success(function(data) {
-        $scope.processInstanceCounts = data;
-      });
-  };
-
-  DashboardController.$inject = ["$scope", "$http", "Uri"];
-
-
-  var Configuration = function Configuration(ViewsProvider) {
-
-    ViewsProvider.registerDefaultView('cockpit.processes.dashboard', {
-      id: 'cockpit-plugin',
-      label: 'Camunda Cockpit Plugin',
-      url: 'plugin://cockpit-plugin/static/app/dashboard.html',
-      controller: DashboardController,
-
-      // make sure we have a higher priority than the default plugin
-      priority: 12
+export default {
+  id: "cockpit-plugin",
+  pluginPoint: "cockpit.processes.dashboard",
+  priority: 12,
+  render: (container, { api }) => {
+    fetch(
+      `${api.cockpitApi}/plugin/cockpit-plugin/${api.engine}/process-instance`
+    ).then(async res => {
+      const processInstanceCounts = await res.json();
+      container.innerHTML = generateHTML(processInstanceCounts);
     });
-  };
-
-  Configuration.$inject = ['ViewsProvider'];
-
-  ngModule.config(Configuration);
-  ngModule.controller('DashboardController', [ '$scope' ]);
-
-  return ngModule;
-});
+  }
+};
